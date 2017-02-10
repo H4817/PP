498125971e79e2d43463b99f94f528ce85c0e43f
JakimychevNikolaj <jakimychev.nikolaj@gmail.com>
2017-02-10 23:02:05 +0300

    process creation test

--- Lab3.1/main.cpp
+++ Lab3.1/main.cpp
@@ -1,21 +1,76 @@
-#include <boost/random/normal_distribution.hpp>
-#include <boost/random/mersenne_twister.hpp>
+#include <iostream>
+#include "PiCalculator.h"
 
-bool inCircle(double x, double y) {
-	return x*x + y*y < 1;
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <spawn.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+void test_fork_exec(void);
+
+void test_posix_spawn(void);
+
+int main(void) {
+    test_fork_exec();
+    test_posix_spawn();
+    return EXIT_SUCCESS;
 }
 
-double GetPi(size_t n) {
-    boost::uniform_01<boost::mt19937> gen((boost::mt19937()));
-    size_t inside = 0;
-
-    for (size_t i = 0; i < n; i++)
-        inside += inCircle(gen(), gen());
-
-    return 4.0 * inside / n;
+void test_fork_exec(void) {
+    pid_t pid;
+    int status;
+    puts("Testing fork/exec");
+    fflush(NULL);
+    pid = fork();
+    switch (pid) {
+        case -1:
+            perror("fork");
+            break;
+        case 0:
+            execl("/bin/ls", "ls", (char *) 0);
+            perror("exec");
+            break;
+        default:
+            printf("Child id: %i\n", pid);
+            fflush(NULL);
+            if (waitpid(pid, &status, 0) != -1) {
+                printf("Child exited with status %i\n", status);
+            } else {
+                perror("waitpid");
+            }
+            break;
+    }
 }
 
-int main() {
-    std::cout << GetPi(10000000) << std::endl;
-    return 0;
+void test_posix_spawn(void) {
+    pid_t pid;
+    char *argv[] = {"ls", (char *) 0};
+    int status;
+    puts("Testing posix_spawn");
+    fflush(NULL);
+    status = posix_spawn(&pid, "/bin/ls", NULL, NULL, argv, environ);
+    if (status == 0) {
+        printf("Child id: %i\n", pid);
+        fflush(NULL);
+        if (waitpid(pid, &status, 0) != -1) {
+            printf("Child exited with status %i\n", status);
+        } else {
+            perror("waitpid");
+        }
+    } else {
+        printf("posix_spawn: %s\n", strerror(status));
+    }
 }
+
+
+
+
+//int main() {
+//    std::cout << PiCalculator::GetPi(10000000) << std::endl;
+//    return 0;
+//}
